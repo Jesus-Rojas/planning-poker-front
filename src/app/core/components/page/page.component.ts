d78eddcfc5bc6c08939301801ecfe51c5856e3ce
Jesus Rojas <jesus@invorious.com>
2024-06-20 13:58:16 -0500

    feat: add styles to loader component and page component :lipstick:

--- src/app/core/components/page/page.component.ts
+++ src/app/core/components/page/page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page',
-  standalone: false,
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })
--- src/app/pages/create-game-page/create-game-page.component.ts
+++ src/app/pages/create-game-page/create-game-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CoreModule } from '../../core/core.module';
+import { DesignSystemModule } from '../../design-system/design-system.module';
 
 @Component({
   selector: 'app-create-game-page',
   standalone: true,
-  imports: [],
+  imports: [CoreModule, DesignSystemModule],
   templateUrl: './create-game-page.component.html',
   styleUrl: './create-game-page.component.scss'
 })

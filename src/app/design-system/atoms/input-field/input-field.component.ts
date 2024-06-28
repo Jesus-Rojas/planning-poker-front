bedaeb5e0c387703f169b1d987082acd661c1a34
Jesus Rojas <jesus@invorious.com>
2024-06-27 23:41:01 -0500

    fix: update logic of input-field :bug:

--- src/app/design-system/atoms/input-field/input-field.component.ts
+++ src/app/design-system/atoms/input-field/input-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { InputFieldVariantEnum } from './types';
 
 @Component({
@@ -6,7 +6,7 @@ import { InputFieldVariantEnum } from './types';
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.scss'
 })
-export class InputFieldComponent {
+export class InputFieldComponent implements OnInit, OnChanges {
   @Input() headerText = '';
   @Input() placeholder = '';
   @Input() value = '';
@@ -18,8 +18,36 @@ export class InputFieldComponent {
   @Input() isValid = false;
   @Output() valueChange = new EventEmitter<string>();
 
+  classDisabled = '';
+  classIsValid = '';
+  classIsInvalid = '';
+
+  updateClassDisabled() {
+    this.classDisabled = this.disabled ? 'disabled' : '';
+  }
+
+  updateClassIsValid() {
+    this.classIsValid = this.isValid ? 'is-valid' : '';
+  }
+
+  updateClassIsInvalid() {
+    this.classIsInvalid = this.isInvalid ? 'is-invalid' : '';
+  }
+
   onInput(event: Event) {
     this.value = (event.target as HTMLInputElement).value;
     this.valueChange.emit(this.value);
+  }
+
+  ngOnInit(): void {
+    this.updateClassDisabled();
+    this.updateClassIsValid();
+    this.updateClassIsInvalid();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['disabled']) this.updateClassDisabled();
+    if (changes['isValid']) this.updateClassIsValid();
+    if (changes['isInvalid']) this.updateClassIsInvalid();
   }
 }
